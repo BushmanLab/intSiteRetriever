1108f17627cc4ea57e3100a694ce0c97b6d9f352
Anatoly Dryga <anatolydryga@gmail.com>
2015-07-22 17:34:46 -0400

    schema with constraints

--- tests/testthat/test_multihit_length.R
+++ tests/testthat/test_multihit_length.R
@@ -1,13 +1,13 @@
 context("check multihit length")
 
-sql_schema <- "../../intsitesdev.sqlite"
+sql_schema <- "../../integration_site_schema.sql"
 db_name <- "test_database"
 system(paste('sqlite3', db_name, '<', sql_schema))
 
 dbConn <- dbConnect(RSQLite::SQLite(), db_name)
 
-dbSendQuery(dbConn, 'INSERT INTO samples VALUES (1, "sample1", "hg18", "m");' )
-dbSendQuery(dbConn, 'INSERT INTO samples VALUES (2, "sample2", "hg18", "m");' )
+dbSendQuery(dbConn, 'INSERT INTO samples VALUES (1, "sample1", "hg18", "m", "asdfas");' )
+dbSendQuery(dbConn, 'INSERT INTO samples VALUES (2, "sample2", "hg18", "m", "asdfas");' )
 
 dbSendQuery(dbConn, 'INSERT INTO multihitpositions VALUES (1, 1, 1234, "chr1", "+");' )
 dbSendQuery(dbConn, 'INSERT INTO multihitpositions VALUES (1, 1, 1234, "chr10", "+");' )
