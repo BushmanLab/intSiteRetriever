e9f33b9ac6222c54b91bd8a88b3a18336b17109e
Anatoly Dryga <anatolydryga@gmail.com>
2015-07-28 11:35:26 -0400

    connection/disconnection is delegated away

--- R/intSiteRetriever.R
+++ R/intSiteRetriever.R
@@ -1,45 +1,12 @@
-.connectToDB <- function(dbConn){
-  if(is.null(dbConn)){
-    all_cons <- dbListConnections(MySQL())
-    for (con in all_cons) {
-      discCon <- dbDisconnect(con)
-    }
-    dbConn <- dbConnect(MySQL(), group="intSitesDev") #~/.my.cnf must be present
-    dbConn
-  }else{
-    dbConn
-  }
-}
-
-#this is a hack-around version of dbQuoteString that does the exact same thing
-#but does not require an instance of DBIConnection to be passed along.  This
-#gets around the issue where we have to SQL quote before a DB connection is
-#established
-.quoteForMySQL <- function(x){
-  x <- gsub("'", "''", x, fixed=TRUE)
-  str <- paste("'", encodeString(x), "'", sep="")
-  str[is.na(x)] <- "NULL"
-  SQL(str)
-}
-
-.disconnectFromDB <- function(dbConn, conn){ #conn is passed in from user, dbConn is the actual connection
-  if(is.null(conn)){ #we made a temp connection which needs to be closed
-    dbDisconnect(dbConn)
-  }
-}
-
 #parse a vector of strings into a piped together list ready for SQL REGEXP
 #is tolerant of MySQL's '%' wildcard which is unfortunately used pretty extensively in legacy code
 #allows single distinct queries
 .parseSetNames <- function(setName){
-  .quoteForMySQL(paste0("^", setName, "$", collapse="|"))
+    stop("obsolete")
 }
 
 .intSiteRetrieverQuery <- function(command, conn){
-  dbConn <- .connectToDB(conn)
-  res <- suppressWarnings(dbGetQuery(dbConn, command))
-  .disconnectFromDB(dbConn, conn)
-  res
+    stop("obsolete")
 }
 
 #' for a given sample names get sites.
